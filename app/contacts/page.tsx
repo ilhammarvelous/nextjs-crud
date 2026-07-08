a6384a3641eb34cc1e4aa1e31e533d4cc1a84f0b
ilham <kurniawanvamoz@gmail.com>
2026-07-08 21:34:51 +0700

    Membuat Fitur Search dan menerapkan use debounce

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -2,14 +2,23 @@ import ContactTable from "@/components/contact-table"
 import Search from "@/components/search"
 import { CreateButton } from "@/components/buttons"
 
-const Contacts = () => {
+type Props = {
+    searchParams: Promise<{ query?: string; page?: string }>;
+};
+
+const Contacts = async ({ searchParams }: Props) => {
+
+    const params = await searchParams;
+    const query = params?.query || "";
+    const currentPage = Number(params?.page) || 1;
+
     return (
         <div className="max-w-screen-md mx-auto mt-5">
             <div className="flex items-center justify-between gap-1 mb-5">
                 <Search />
                 <CreateButton />
             </div>
-            <ContactTable />
+            <ContactTable query={query} currentPage={currentPage} />
         </div>
     )
 }
